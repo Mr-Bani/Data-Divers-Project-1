904bad7029e28be73119136f4201bbb8d16c7c87
Mahdi <mahdi.bani.2003@gmail.com>
2023-02-06 00:30:14 +0400

    extract_links is a function now

--- extract_links.py
+++ extract_links.py
@@ -5,25 +5,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
-options = Options()
-options.add_argument('--headless')
+def links_in_page(url):
+    options = Options()
+    options.add_argument('--headless')
 
-driver = webdriver.Chrome(options=options)
-driver.get('https://www.delino.com/search?q=%D9%BE%DB%8C%D8%AA%D8%B2%D8%A7')
-driver.implicitly_wait(6)
-result_number = int(driver.find_element(By.CSS_SELECTOR, '.result-container header>small').text.split()[0])
-print(f'Expected number of restaurants: {result_number}')
-print('Scrapping...')
+    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Firefox()
+    driver.get(url)
+    driver.implicitly_wait(6)
+    result_number = int(driver.find_element(By.CSS_SELECTOR, '.result-container header>small').text.split()[0])
+    print(f'Expected number of restaurants: {result_number}')
+    print('Scrapping...')
 
-wait = WebDriverWait(driver, 5)
-try:
-    while True:
-        button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.load-more-holder button')))
-        driver.execute_script("arguments[0].click();", button)
-except TimeoutException:
-    pass
+    wait = WebDriverWait(driver, 5)
+    try:
+        while True:
+            button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.load-more-holder button')))
+            driver.execute_script("arguments[0].click();", button)
+    except TimeoutException:
+        pass
 
-restaurants = driver.find_element(By.CLASS_NAME, 'rest-list.clearfix').find_elements(By.CLASS_NAME ,'r-i')
-links = [restaurant.get_attribute('href') for restaurant in restaurants]
-print(f'Scrapping finished.')
-print(f'Number of links: {len(links)}')
+    restaurants = driver.find_element(By.CLASS_NAME, 'rest-list.clearfix').find_elements(By.CLASS_NAME ,'r-i')
+    links = [restaurant.get_attribute('href') for restaurant in restaurants]
+    print(f'Scrapping finished.')
+    print(f'Number of links: {len(links)}')
+    return links
